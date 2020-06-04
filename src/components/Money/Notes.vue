96284499058d6a8be8c103e6089b616a79ecb060
YoungDriverOfTech <xiaoleliang045@gmail.com>
2020-06-04 16:24:02 +0900

    Money页面JS完成

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,29 +1,40 @@
 <template>
     <div>
         <label class="notes">
-            <span class="name">备注</span>
-            <input type="text" placeholder="在这里输入备注"/>
+            <span class="name">备注 {{value}}</span>
+            <input type="text" v-model="value" placeholder="在这里输入备注"/>
         </label>
     </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Notes'
-  };
+  import Vue from 'vue';
+  import {Component, Watch} from 'vue-property-decorator';
+
+  @Component
+  export default class Notes extends Vue {
+    value = '';
+
+    @Watch('value')
+    onValueChanged(value: string) {
+      this.$emit('update:value', value);
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
-    .notes{
+    .notes {
         font-size: 14px;
         background: #f5f5f5;
         padding-left: 16px;
         display: flex;
         align-items: center;
-        .name{
+
+        .name {
             padding-right: 16px;
         }
-        input{
+
+        input {
             height: 64px;
             flex-grow: 1;
             background: transparent;
--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,44 +1,49 @@
 <template>
     <div>
         <ul class="types">
-            <li :class="type === '-' && 'selected'"
-                @click="selectType('-')">支出</li>
-            <li :class="type === '+' && 'selected'"
-                @click="selectType('+')">收入</li>
+            <li :class="value === '-' && 'selected'"
+                @click="selectType('-')">支出
+            </li>
+            <li :class="value === '+' && 'selected'"
+                @click="selectType('+')">收入
+            </li>
         </ul>
     </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
 
   @Component
-  export default class Types extends Vue{
-    type = '-';  // - 表示支出， +表示收入
-    selectType(type: string){
-      if (type !== '-' && type !== '+'){
-        throw new Error('Unknown type')
+  export default class Types extends Vue {
+    @Prop() readonly value!: string;
+
+    selectType(type: string) {
+      if (type !== '-' && type !== '+') {
+        throw new Error('Unknown type');
       }
-      this.type = type;
+      this.$emit('update:value', type);
     }
   }
 </script>
 
 <style lang="scss" scoped>
-    .types{
+    .types {
         background: #c4c4c4;
         display: flex;
         text-align: center;
         font-size: 24px;
-        > li{
+
+        > li {
             width: 50%;
             height: 64px;
             display: flex;
             justify-content: center;
             align-items: center;
             position: relative;
-            &.selected::after{
+
+            &.selected::after {
                 content: '';
                 position: absolute;
                 bottom: 0;
