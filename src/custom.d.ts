d9963a6f633e876425bb81e54c37c249b2b883e1
YoungDriverOfTech <xiaoleliang045@gmail.com>
2020-06-05 16:53:26 +0900

    封装taglist的增删改查

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -21,5 +21,8 @@ type TagListModel = {
 
 interface Window {
   tagList: Tag[];
+  findTag: (id: string) => Tag;
   createTag: (name: string) => void;
+  removeTag: (id: string) => boolean;
+  updateTag: TagListModel['update'];
 }
