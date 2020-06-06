f9ca4e1a1b0c6640695ab268e546852656a39b36
YoungDriverOfTech <xiaoleliang045@gmail.com>
2020-06-06 10:40:34 +0900

    统计界面HTML CSS

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <ul class="types">
-      <li :class="value === '-' && 'selected'"
+      <li :class="{[classPrefix+'-item']: classPrefix, selected: value === '-'}"
           @click="selectType('-')">支出
       </li>
-      <li :class="value === '+' && 'selected'"
+      <li :class="{[classPrefix+'-item']: classPrefix, selected: value === '+'}"
           @click="selectType('+')">收入
       </li>
     </ul>
@@ -17,7 +17,8 @@
 
   @Component
   export default class Types extends Vue {
-    @Prop() readonly value!: string;
+    @Prop(String) readonly value!: string;
+    @Prop(String) classPrefix?: string
 
     selectType(type: string) {
       if (type !== '-' && type !== '+') {
@@ -48,7 +49,7 @@
         left: 0;
         width: 100%;
         height: 4px;
-        background: #333;
+        background: RGB(16,55,175);;
       }
     }
   }
